168268266a23b3724d2dae9474beb95040110798
gris <185072671+d-branco@users.noreply.github.com>
2024-12-04 10:10:40 +0000

    formatting

--- day03.c
+++ day03.c
@@ -6,13 +6,12 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 08:49:26 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/12/03 10:31:56 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/12/04 10:06:31 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // usage 
 //	cc -Wall -Werror -Wextra day03.c input03.h && ./a.out
-
 
 #include "input03.h"
 //	unsigned long	input_addition(void)
@@ -33,7 +32,7 @@ int	main(void)
 
 unsigned long	ft_mul(int a, int b)
 {
-	unsigned long result;
+	unsigned long	result;
 
 	result = a * b;
 	return (result);
--- day04.c
+++ day04.c
@@ -6,7 +6,7 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 08:30:40 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/12/04 09:47:27 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/12/04 10:09:58 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,27 @@ int	main(void)
 	int	i;
 	int	j;
 	int	appearances;
-	
+
 	appearances = 0;
 	// HORIZONTAL
 	i = 0;
 	j = 0;
-	while(j < 140)
+	while (j < 140)
 	{
 		i = 0;
-		while(i < 140)
+		while (i < 140)
 		{
 			if ((j + 3) >= 140)
 				break ;
-			else if (((matrix[i][j] == 'X') && matrix[i][j + 1] == 'M' && matrix[i][j + 2] == 'A' && matrix[i][j + 3] == 'S')
-				|| ((matrix[i][j] == 'S') && matrix[i][j + 1] == 'A' && matrix[i][j + 2] == 'M' && matrix[i][j + 3] == 'X'))
+			else if (((matrix[i][j] == 'X') && matrix[i][j + 1] == 'M'
+				&& matrix[i][j + 2] == 'A' && matrix[i][j + 3] == 'S')
+				|| ((matrix[i][j] == 'S') && matrix[i][j + 1] == 'A'
+				&& matrix[i][j + 2] == 'M' && matrix[i][j + 3] == 'X'))
 			{
 				appearances++;
-				//printf("Located at matrix[%d][%d] %c%c%c%c\n", i, j, matrix[i][j], matrix[i][j + 1], matrix[i][j + 2], matrix[i][j + 3]);
+				//printf("Located at matrix[%d][%d] %c%c%c%c\n", i, j,
+				//	matrix[i][j], matrix[i][j + 1], matrix[i][j + 2],
+				//	matrix[i][j + 3]);
 			}
 			i++;
 		}
@@ -46,18 +50,22 @@ int	main(void)
 	// VERTICAL
 	i = 0;
 	j = 0;
-	while(j < 140)
+	while (j < 140)
 	{
 		i = 0;
-		while(i < 140)
+		while (i < 140)
 		{
 			if ((i + 3) >= 140)
 				i = 140 ;
-			else if (((matrix[i][j] == 'X') && matrix[i + 1][j] == 'M' && matrix[i + 2][j] == 'A' && matrix[i + 3][j] == 'S')
-				|| ((matrix[i][j] == 'S') && matrix[i + 1][j] == 'A' && matrix[i + 2][j] == 'M' && matrix[i + 3][j] == 'X'))
+			else if (((matrix[i][j] == 'X') && matrix[i + 1][j] == 'M'
+				&& matrix[i + 2][j] == 'A' && matrix[i + 3][j] == 'S')
+				|| ((matrix[i][j] == 'S') && matrix[i + 1][j] == 'A'
+				&& matrix[i + 2][j] == 'M' && matrix[i + 3][j] == 'X'))
 			{
 				appearances++;
-				//printf("Located at matrix[%d][%d] %c%c%c%c\n", i, j, matrix[i][j], matrix[i + 1][j], matrix[i + 2][j], matrix[i + 3][j]);
+				//printf("Located at matrix[%d][%d] %c%c%c%c\n", i, j,
+				//	matrix[i][j], matrix[i + 1][j], matrix[i + 2][j],
+				//	matrix[i + 3][j]);
 			}
 			i++;
 		}
@@ -66,17 +74,19 @@ int	main(void)
 	// downward diagonal
 	i = 0;
 	j = 0;
-	while(j < 140)
+	while (j < 140)
 	{
 		i = 0;
-		while(i < 140)
+		while (i < 140)
 		{
 			if ((i + 3) >= 140)
 				i = 140;
 			else if ((j + 3) >= 140)
 				break ;
-			else if (((matrix[i][j] == 'X') && matrix[i + 1][j + 1] == 'M' && matrix[i + 2][j + 2] == 'A' && matrix[i + 3][j + 3] == 'S')
-				|| ((matrix[i][j] == 'S') && matrix[i + 1][j + 1] == 'A' && matrix[i + 2][j + 2] == 'M' && matrix[i + 3][j + 3] == 'X'))
+			else if (((matrix[i][j] == 'X') && matrix[i + 1][j + 1] == 'M'
+				&& matrix[i + 2][j + 2] == 'A' && matrix[i + 3][j + 3] == 'S')
+				|| ((matrix[i][j] == 'S') && matrix[i + 1][j + 1] == 'A'
+				&& matrix[i + 2][j + 2] == 'M' && matrix[i + 3][j + 3] == 'X'))
 			{
 				appearances++;
 				//printf("Located at matrix[%d][%d]\n", i, j);
@@ -88,17 +98,19 @@ int	main(void)
 	// forward diagonal
 	i = 0;
 	j = 0;
-	while(j < 140)
+	while (j < 140)
 	{
 		i = 0;
-		while(i < 140)
+		while (i < 140)
 		{
 			if ((i + 3) >= 140)
 				i = 140;
 			else if ((j + 3) >= 140)
 				break ;
-			else if (((matrix[i + 3][j] == 'X') && matrix[i + 2][j + 1] == 'M' && matrix[i + 1][j + 2] == 'A' && matrix[i][j + 3] == 'S')
-				|| ((matrix[i + 3][j] == 'S') && matrix[i + 2][j + 1] == 'A' && matrix[i + 1][j + 2] == 'M' && matrix[i][j + 3] == 'X'))
+			else if (((matrix[i + 3][j] == 'X') && matrix[i + 2][j + 1] == 'M'
+				&& matrix[i + 1][j + 2] == 'A' && matrix[i][j + 3] == 'S')
+				|| ((matrix[i + 3][j] == 'S') && matrix[i + 2][j + 1] == 'A'
+				&& matrix[i + 1][j + 2] == 'M' && matrix[i][j + 3] == 'X'))
 			{
 				appearances++;
 				//printf("Located at matrix[%d][%d]\n", i, j);
@@ -108,16 +120,14 @@ int	main(void)
 		j++;
 	}
 	printf("XMAS appearances: %d\n", appearances); //2569
-
 	// SECOND PART
 	appearances = 0;
-	// MM on top
 	i = 0;
 	j = 0;
-	while(j < 140)
+	while (j < 140)
 	{
 		i = 0;
-		while(i < 140)
+		while (i < 140)
 		{
 			if ((i + 2) >= 140)
 				i = 140;
@@ -143,6 +153,6 @@ int	main(void)
 		}
 		j++;
 	}
-	printf("X-MAS appearances: %d\n", appearances); //2569
+	printf("X-MAS appearances: %d\n", appearances); //1998
 	return (0);
 }
