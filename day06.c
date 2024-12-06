7327a83ad5df4b98d89965f9a39d6a342a576797
gris <185072671+d-branco@users.noreply.github.com>
2024-12-06 16:29:28 +0000

    both parts

--- day06.c
+++ day06.c
@@ -6,7 +6,7 @@
 /*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 07:51:30 by abessa-m          #+#    #+#             */
-/*   Updated: 2024/12/06 16:17:12 by abessa-m         ###   ########.fr       */
+/*   Updated: 2024/12/06 16:29:03 by abessa-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,15 +34,15 @@ int		check_if_it_loops(char grid[130][130], int obs_x, int obs_y);
 
 int	main(void)
 {
-	//t_guard	guard;
-	//int		steps;
+	t_guard	guard;
+	int		steps;
 	int		loop;
 	int		x;
 	int		y;
 	int		count;
+	char	grid[130][130];
 
 	// EXAMPLE
-	/*
 	steps = 1;
 	print_example_grid(example);
 	// pos_x    pos_y    dir_x    dir_y
@@ -110,6 +110,17 @@ int	main(void)
 	print_example_grid(example);
 	printf("Steps: %d\n\n", steps);
 	//	PART ONE
+	y = 0;
+	while (y < 130)
+	{
+		x = 0;
+		while (x < 130)
+		{
+			grid[y][x] = first[y][x];
+			x++;
+		}
+		y++;
+	}
 	steps = 1;
 	print_first_grid(first);
 	// pos_x    pos_y    dir_x    dir_y
@@ -119,23 +130,23 @@ int	main(void)
 	guard.dir_y = -1;
 	while ((guard.x > 0 && guard.x < 130) && (guard.y > 0 && guard.y < 130))
 	{
-		while ((first[guard.y + guard.dir_y][guard.x + guard.dir_x] == '.')
-			|| (first[guard.y + guard.dir_y][guard.x + guard.dir_x] == '<')
-			|| (first[guard.y + guard.dir_y][guard.x + guard.dir_x] == '>')
-			|| (first[guard.y + guard.dir_y][guard.x + guard.dir_x] == 'v')
-			|| (first[guard.y + guard.dir_y][guard.x + guard.dir_x] == '^')
+		while ((grid[guard.y + guard.dir_y][guard.x + guard.dir_x] == '.')
+			|| (grid[guard.y + guard.dir_y][guard.x + guard.dir_x] == '<')
+			|| (grid[guard.y + guard.dir_y][guard.x + guard.dir_x] == '>')
+			|| (grid[guard.y + guard.dir_y][guard.x + guard.dir_x] == 'v')
+			|| (grid[guard.y + guard.dir_y][guard.x + guard.dir_x] == '^')
 			)
 		{
-			if (first[guard.y + guard.dir_y][guard.x + guard.dir_x] == '.')
+			if (grid[guard.y + guard.dir_y][guard.x + guard.dir_x] == '.')
 				steps++;
 			if (guard.dir_x == 1)
-				first[guard.y][guard.x] = '>';
+				grid[guard.y][guard.x] = '>';
 			if (guard.dir_x == -1)
-				first[guard.y][guard.x] = '<';
+				grid[guard.y][guard.x] = '<';
 			if (guard.dir_y == 1)
-				first[guard.y][guard.x] = 'v';
+				grid[guard.y][guard.x] = 'v';
 			if (guard.dir_y == -1)
-				first[guard.y][guard.x] = '^';
+				grid[guard.y][guard.x] = '^';
 			guard.x += guard.dir_x;
 			guard.y += guard.dir_y;
 		}
@@ -147,9 +158,9 @@ int	main(void)
 			guard.y += guard.dir_y;
 			break ;
 		}
-		first[guard.y][guard.x] = '@';
+		grid[guard.y][guard.x] = '@';
 		//printf("\n");
-		//print_first_grid(first);
+		//print_first_grid(grid);
 		//rotate tau / 4
 		if (guard.dir_x == 1)
 		{
@@ -174,10 +185,9 @@ int	main(void)
 	//printf("Steps: %d\n", steps);
 	}
 	printf("\n");
-	print_first_grid(first);
+	print_first_grid(grid);
 	printf("Steps: %d\n\n", steps);
 	// 5095
-	*/
 	//	PART TWO
 	count = 0;
 	y = 0;
@@ -206,10 +216,11 @@ int	main(void)
 	}
 	// 247 TOO LOW
 	// 621 TOO LOW
-	printf("\nCount: %d\n", count);
+	printf("Count: %d\n", count);
 	//2128 TOO HIGH
 	//1937 wrong
 	//1939 wrong but coincides with someone else
+	// 1933
 	return (0);
 }
 
@@ -221,12 +232,10 @@ int	check_if_it_loops(char grid[130][130], int obs_x, int obs_y)
 	int		x;
 	int		y;
 	char	first[130][130];
-	//int		old_steps;
 	int		iter;
 
 	iter = 0;
 	steps = 1;
-	//old_steps = 1;
 	y = 0;
 	while (y < 130)
 	{
@@ -260,22 +269,22 @@ int	check_if_it_loops(char grid[130][130], int obs_x, int obs_y)
 		{
 			if (first[guard.y + guard.dir_y][guard.x + guard.dir_x] == '.')
 				steps++;
-			else if (guard.dir_x == 1) 
+			else if (guard.dir_x == 1)
 			{
 				if (first[guard.y][guard.x] == '.')
 					first[guard.y][guard.x] = '>';
 			}
-			else if (guard.dir_x == -1) 
+			else if (guard.dir_x == -1)
 			{
 				if (first[guard.y][guard.x] == '.')
 					first[guard.y][guard.x] = '<';
 			}
-			else if (guard.dir_y == 1) 
+			else if (guard.dir_y == 1)
 			{
 				if (first[guard.y][guard.x] == '.')
 					first[guard.y][guard.x] = 'v';
 			}
-			else if (guard.dir_y == -1) 
+			else if (guard.dir_y == -1)
 			{
 				if (first[guard.y][guard.x] == '.')
 					first[guard.y][guard.x] = '^';
@@ -289,7 +298,7 @@ int	check_if_it_loops(char grid[130][130], int obs_x, int obs_y)
 				break ;
 		}
 		iter++;
-		if (iter > 20000)//////////////////////////////////////// ITERATOR LIMIT
+		if (iter > 2000) /////////////////////////////////////// ITERATOR LIMIT
 		{
 			//printf("\n");
 			//print_first_grid(first);
@@ -309,7 +318,7 @@ int	check_if_it_loops(char grid[130][130], int obs_x, int obs_y)
 		//printf("\n");
 		//print_first_grid(first);
 		//rotate tau / 4
-		if (guard.dir_x == 1) 
+		if (guard.dir_x == 1)
 		{
 			guard.dir_x = 0;
 			guard.dir_y = 1;
