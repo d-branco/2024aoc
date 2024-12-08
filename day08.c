5d1b11eab911d0f341105b2658e3a3c8574a8ba5
gris <185072671+d-branco@users.noreply.github.com>
2024-12-08 10:59:08 +0000

    initial

---
+++ day08.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   day08.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abessa-m <abessa-m@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/08 10:50:26 by abessa-m          #+#    #+#             */
+/*   Updated: 2024/12/08 10:58:56 by abessa-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// usage 
+//	cc -Wall -Werror -Wextra day08.c input08.h && ./a.out
+
+#include "input08.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*freq;
+		
+	freq = "
+		0123456789
+		abcdefghijklmnopqrstuvwxyz
+		ABCDEFGHIJKLMNOPQRSTUVWXYZ
+		";
+	
+}
